2d74a2af71e28aa143a3da7d6c9320665b3913e8
PolugaGit <77752971+PolugaGit@users.noreply.github.com>
2024-09-28 12:08:47 -0500

    Implemented Finite State Machine functioanlity
    implemented a finite state machine for the bird GameObject.

---
+++ Assets/Scripts/bird_states/BFlying.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BFlying : IState
+{
+    private BirdReferences bR;
+
+    public BFlying(BirdReferences birdReferences) {
+        this.bR = birdReferences;
+    }
+
+    public void OnEnter() {
+
+    }
+
+    public void OnExit() {
+
+    }
+
+    public void Tick() {
+
+    }
+}
